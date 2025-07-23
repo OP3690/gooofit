6905545dbcbcca6ca5204979358cebb07f477e31
OP3690 <omprakashutaha@gmail.com>
2025-07-23 20:33:51 +0530

    Enhanced Forgot Password UI with Mobile SMS Integration and Fixed Mobile Tile Symmetry

--- server.js
+++ server.js
@@ -23,7 +23,14 @@ app.use(cors({
         'https://weight-management-client.vercel.app',
         'http://localhost:3000'
       ]
-    : 'http://localhost:3000',
+    : [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3002',
+        'http://localhost:3003',
+        'http://localhost:3004',
+        'http://localhost:3005'
+      ],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
